8682ba709fda0454f7b4ef6500ac05441b5eb18d
siddhantprateek <siddhantprateek@gmail.com>
2023-09-13 20:49:19 +0530

    ft(comm_svc): comm svc entrypoint

--- comm_svc/src/app.module.ts
+++ comm_svc/src/app.module.ts
@@ -3,16 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MorganModule, MorganInterceptor } from 'nest-morgan';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MorganModule],
+  imports: [MorganModule, UsersModule],
   controllers: [AppController],
   providers: [
     {
       provide: APP_INTERCEPTOR,
       useClass: MorganInterceptor("combined"),
     },
-    AppService,
+    AppService
   ],
 })
 export class AppModule {}
