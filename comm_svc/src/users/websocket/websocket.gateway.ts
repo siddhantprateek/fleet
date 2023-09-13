40d3cd7d090bccb369491f84edfa15efbf4e7d2a
siddhantprateek <siddhantprateek@gmail.com>
2023-09-13 22:39:09 +0530

    ft(commsvc): websocket rooms

--- comm_svc/src/users/websocket/websocket.gateway.ts
+++ comm_svc/src/users/websocket/websocket.gateway.ts
@@ -1,20 +1,25 @@
 import { OnModuleInit } from '@nestjs/common';
 import { WebSocketGateway, WebSocketServer, SubscribeMessage, MessageBody } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class WebsocketGateway {
+@WebSocketGateway({
+  cors: {
+    origin: ['http://localhost:3000'],
+  },
+})
+export class WebsocketGateway implements OnModuleInit {
   @WebSocketServer() server: Server;
 
   onModuleInit() {
-    this.server.on('connection', (socket) => {
-      console.log(socket.id);
-      console.log('Connected');
+    this.server.on('connection', (socket: Socket) => {
+      console.log(`Client connected: ${socket.id}`);
+      console.log('Socket Connected');
+      socket.join(['room1', 'room2'])
     });
   }
 
   @SubscribeMessage('serviceHit')
   handleServiceHit(@MessageBody() data: string): void {
-    this.server.emit('serviceHit', data);
+    this.server.to('room1').emit('serviceHit', data);
   }
 }
