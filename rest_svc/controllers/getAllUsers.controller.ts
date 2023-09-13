44c381b9b1cceaf776a49d1b925e59ef9a2af358
siddhantprateek <siddhantprateek@gmail.com>
2023-09-13 12:58:48 +0530

    ft(rest): user controller

---
+++ rest_svc/controllers/getAllUsers.controller.ts
@@ -0,0 +1,54 @@
+import { Request, Response } from "express";
+import { Prisma } from "@prisma/client"; // Import Prisma
+import { prisma } from "../config/postgres/postgres";
+
+const GetAllUser = async (req: Request, res: Response) => {
+  const page = parseInt(req.query.page as string) || 1; // Get the requested page number (default to 1)
+  const pageSize = parseInt(req.query.pageSize as string) || 10; // Get the page size (default to 10)
+  const sortBy = (req.query.sortBy as string) || "id"; // Get the sorting field (default to 'id')
+  const sortOrder = (req.query.sortOrder as "asc" | "desc") || "asc"; // Get the sorting order (default to 'asc')
+
+  // Define valid sorting fields
+  const validSortFields: Record<
+    string,
+    keyof Prisma.userOrderByWithRelationInput
+  > = {
+    id: "id",
+    name: "name",
+    email: "email",
+  };
+
+  try {
+    const orderBy: Prisma.userOrderByWithRelationInput = {};
+    if (validSortFields[sortBy]) {
+      orderBy[validSortFields[sortBy]] = sortOrder;
+    } else {
+
+      orderBy.id = sortOrder;
+    }
+    const skip = (page - 1) * pageSize;
+
+    const users = await prisma.user.findMany({
+      skip,
+      take: pageSize,
+      orderBy,
+    });
+    const totalCount = await prisma.user.count();
+
+    res.status(200).json({
+      message: "Users retrieved successfully",
+      users,
+      pagination: {
+        page,
+        pageSize,
+        totalPages: Math.ceil(totalCount / pageSize),
+        totalItems: totalCount,
+      },
+    });
+  } catch (error) {
+    console.error("Error retrieving users:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export default GetAllUser;
---
+++ rest_svc/controllers/user.controller.ts
@@ -0,0 +1,169 @@
+import { Request, Response } from "express";
+import { prisma } from "../config/postgres/postgres";
+import { IUserCreate, IUserLogin } from "../types/user.types";
+import { validationResult } from "express-validator";
+import { Prisma } from "@prisma/client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+
+const HashPassword = async (password: string) => {
+  return bcrypt.hash(password, 10);
+};
+
+// Handle User registration
+// @params: null
+// @method: POST
+// @route: /api/register
+const CreateUser = async (req: Request, res: Response) => {
+  const { name, email, password, role, company }: IUserCreate = req.body;
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(406).json({
+        message: "Invalid data",
+        errors,
+      });
+    }
+    let hashedPassword = await HashPassword(password);
+    let date = new Date()
+    const userData: Prisma.userCreateInput = {
+      name: name,
+      email: email,
+      password: hashedPassword,
+      role: role,
+      company: company,
+      createdAt: date,
+    };
+
+    const user = await prisma.user.create({
+      data: userData,
+    });
+    res.status(201).json({
+      message: "User created successfully",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+      error
+    });
+  }
+};
+
+let AuthenticateUser = async (req: Request, res: Response) => {
+  const { email, password }: IUserLogin = req.body;
+
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(406).json({
+        message: "Invalid Data",
+        errors,
+      });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      res.status(401).json({
+        message: "User not found",
+      });
+    }
+
+    const isPasswordMatching = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!isPasswordMatching) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign(
+      { userId: user?.id, userEmail: user?.email },
+      process.env.JWT_SECRET || "default",
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({
+      message: "Authentication successful",
+      token,
+    });
+  } catch (error) {
+    console.error("Error authenticating user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+let UpdateUser = async (req: Request, res: Response) => {};
+
+let DeleteUser = async (req: Request, res: Response) => {
+  const userId = req.params.id; // Assuming you have a route parameter for the user ID
+
+  try {
+    const existingUser = await prisma.user.findUnique({
+      where: { id: Number(userId) },
+    });
+
+    if (!existingUser) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    // Delete the user
+    await prisma.user.delete({
+      where: { id: Number(userId) },
+    });
+
+    res.status(200).json({
+      message: "User deleted successfully",
+    });
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+let GetUser = async (req: Request, res: Response) => {
+  const userId = req.params.id;
+  try {
+    // Find the user with the specified ID
+    const user = await prisma.user.findUnique({
+      where: { id: Number(userId) },
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "User retrieved successfully",
+      user,
+    });
+  } catch (error) {
+    console.error("Error retrieving user:", error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+
+export { 
+    DeleteUser,
+    CreateUser,
+    AuthenticateUser,
+    UpdateUser,
+    GetUser
+}
