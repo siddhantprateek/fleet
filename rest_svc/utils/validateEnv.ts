2f47c70d903121d089b1129c4d0f48c90eb34e06
siddhantprateek <siddhantprateek@gmail.com>
2023-09-13 12:57:39 +0530

    ft(rest): types

---
+++ rest_svc/utils/validateEnv.ts
@@ -0,0 +1,10 @@
+import { cleanEnv, port, str } from "envalid";
+
+function validateEnv() {
+  cleanEnv(process.env, {
+    JWT_SECRET: str(),
+    PORT: port(),
+  });
+}
+
+export default validateEnv;
