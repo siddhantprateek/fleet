a2645c0cebfc01a138cfef7fa4b365b9219079d5
siddhantprateek <siddhantprateek@gmail.com>
2023-09-14 08:59:21 +0530

    ft(db): read from .env file

--- comm_svc/src/database/database.providers.ts
+++ comm_svc/src/database/database.providers.ts
@@ -4,6 +4,6 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: async (): Promise<typeof mongoose> =>
-      await mongoose.connect('mongodb://root:root@localhost:27017'),
+      await mongoose.connect(process.env.MONGO_URI),
   },
 ];
