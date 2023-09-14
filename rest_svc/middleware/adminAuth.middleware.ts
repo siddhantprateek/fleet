b288894c59638f53b15a3e1d92007e8556fe8c3f
siddhantprateek <siddhantprateek@gmail.com>
2023-09-14 10:56:45 +0530

    ft(middleware): auth and authorization integrated

--- rest_svc/middleware/adminAuth.middleware.ts
+++ rest_svc/middleware/adminAuth.middleware.ts
@@ -1,18 +1,46 @@
 import { Request, Response, NextFunction } from "express";
+import { prisma } from "../config/postgres/postgres";
+import jwt from "jsonwebtoken";
+import { IDecode } from "../types/user.types";
 
-const isAdmin = (req: Request, res: Response, next: NextFunction) => {
-  const userRole = req.body.user.role;
+const isAdmin = async (req: Request, res: Response, next: NextFunction) => {
+ 
+  try {
+    let email: string;
+    if (
+      req.headers &&
+      req.headers.authorization &&
+      req.headers.authorization.split(" ")[0] === "jwt"
+    ) {
+      const token = req.headers.authorization!.split(" ")[1];
+      let decode = jwt.verify(token, process.env.JWT_SECRET || "default") as IDecode;
+      email = decode.userEmail
 
-  if (userRole === "admin") {
-    // User has admin role, allow access to the route
-    next();
-  } else {
-    // User does not have admin role,
-    // deny access with a 403 Forbidden response
-    res.status(403).json({
-      message: "Access denied: You must be an admin to access this resource",
-    });
+      let user = await prisma.user.findUnique({
+        where: {
+          email
+        }
+      })
+
+      let userRole = user?.role
+      if (userRole === "admin") {
+        next();
+      } else {
+        // User does not have admin role,
+        // deny access with a 403 Forbidden response
+        res.status(403).json({
+          message: "Access denied: You must be an admin privilages to access this resource",
+        });
+      }
+    }
+  } catch (error) {
+    return res.status(401).json({
+      "message": "Access denied: You must be an admin privilages to access this resource"
+    })
   }
+
+
+
 };
 
 export default isAdmin;
--- rest_svc/middleware/authorize.middleware.ts
+++ rest_svc/middleware/authorize.middleware.ts
@@ -1,17 +1,28 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
+import { IDecode, IUserLogin } from "../types/user.types";
+
 dotenv.config();
 
-const authorize = (req: Request, res: Response, next: NextFunction) => {
+const authorize = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const token = req.headers.authorization!.split(" ")[1];
-    const decode = jwt.verify(token, process.env.JWT_SECRET || "default");
-    req.body.user = decode;
+    let email: string;
+    if (
+      req.headers &&
+      req.headers.authorization &&
+      req.headers.authorization.split(" ")[0] === "jwt"
+    ) {
+      const token = req.headers.authorization!.split(" ")[1];
+      let decode = jwt.verify(token, process.env.JWT_SECRET || "default") as IDecode;
+      email = decode.userEmail
+    }
     next();
   } catch (error) {
     req.body.user = undefined;
-    next();
+    return res.status(401).json({
+      "message": "Unauthorize access to api."
+    })
   }
 };
 
