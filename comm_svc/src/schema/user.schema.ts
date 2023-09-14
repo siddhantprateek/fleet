4b323002b112768245eee69d0028a11ba92b49d8
siddhantprateek <siddhantprateek@gmail.com>
2023-09-14 08:59:34 +0530

    ft(schema): schema validation

--- comm_svc/src/schema/user.schema.ts
+++ comm_svc/src/schema/user.schema.ts
@@ -11,12 +11,15 @@ export const UserSchema = new Schema({
     },
     email: {
         type: String,
+        required: true,
     }, 
     password: {
         type: String,
+        required: true,
     },
     role: {
         type: String,
+        required: true,
     },
     comapany: {
         type: String,
