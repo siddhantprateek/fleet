bc47a4c77296cd1d90aae126fbe62d116c32c3d3
siddhantprateek <siddhantprateek@gmail.com>
2023-09-14 10:57:29 +0530

    ft(schema): schema validation

--- comm_svc/src/schema/user.schema.ts
+++ comm_svc/src/schema/user.schema.ts
@@ -3,29 +3,37 @@ import mongoose from 'mongoose';
 let Schema = mongoose.Schema;
 
 export const UserSchema = new Schema({
-    name: {
-        type: String,
+  name: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+  email: {
+    type: String,
+    unique: [true, 'email already exists in database!'],
+    required: [true, 'email not provided'],
+    validate: {
+      validator: function (v) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+      },
+      message: '{VALUE} is not a valid email!',
     },
-    age: {
-        type: Number,
-    },
-    email: {
-        type: String,
-        required: true,
-    }, 
-    password: {
-        type: String,
-        required: true,
-    },
-    role: {
-        type: String,
-        required: true,
-    },
-    comapany: {
-        type: String,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now 
-    }
-})
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["dev", "admin"],
+    required: [true, "Please specify user role"]
+  },
+  comapany: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
